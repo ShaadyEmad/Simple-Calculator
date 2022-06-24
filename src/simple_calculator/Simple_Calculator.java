91f631c67d733dbddbdb107c6698ddcd2a8b7eb1
Shady Emad <95425147+ShaadyEmad@users.noreply.github.com>
2022-06-24 17:08:36 +0200

    Add files via upload

---
+++ src/simple_calculator/Simple_Calculator.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package simple_calculator;
+
+/**
+ *
+ * @author Shady Emad
+ */
+public class Simple_Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Calculator().setVisible(true);
+    }
+    
+}
